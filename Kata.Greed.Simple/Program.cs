38638540be770353fe5a4906febfa0f46ed8c110
wordracr <technologyryan@outlook.com>
2015-08-04 07:20:20 -0500

    Adding TestScoreSixOfAny3000 without code in place

--- Kata.Greed.Simple/Program.cs
+++ Kata.Greed.Simple/Program.cs
@@ -45,17 +45,22 @@ namespace Kata.Greed.Simple
             return diceMade;
         }
 
-        private static int ScoreRoll(int[] dies)
+        public static int ScoreRoll(int[] dies)
         {
-            return 25;
             //TODO: All scoring, one by one.
             int score = 0;
 #region Scoring for 6 dice
-		
+
+            //Sort up front for all scoring so they don't have to all sort.
+            Array.Sort(dies);
+
             //Score full straight. 1500. I'd like to make the decision to put both the  of "contains straight" and points in the same method for simplicity sake. It would be more reusable to put in 2, but is "ContainsStraight" valuable?
             score += ScoreFullStraight(dies);
             //could have some type of escape here to not score anymore if score > 0. How to make that logic reusable though?
-            
+
+            //Six of any number: 3000
+            score += ScoreSixOfAny(dies);
+
             //2 triplets. 2500
             score += ScoreTwoTriplets(dies);
 
@@ -63,12 +68,12 @@ namespace Kata.Greed.Simple
             score += ScoreFourOfAnyWithAPair(dies);
             //Three pairs: 1500
             score += ScoreThreePairs(dies);
-            //Six of any number: 3000
-            score += ScoreSixOfAny(dies);
 
             if (score > 0)
                 return score;
 	#endregion
+
+            return 25;
 
             //remember to remove used dice
             //Needed though? Yes.. for something like {1,1,1,1,5,5}, 4 ones would be 1000, and they shouldn't be counted again when doing ones.
@@ -98,7 +103,12 @@ namespace Kata.Greed.Simple
 
         private static int ScoreFullStraight(int[] dies)
         {
-            throw new NotImplementedException();
+            var exampleStraight = Enumerable.Range(1, 6).ToArray();
+            if (exampleStraight.SequenceEqual(dies))
+            {
+                return 1500;
+            }
+            return 0;
         }
 
         private static int ScoreThreeOfAny(int[] dies)
